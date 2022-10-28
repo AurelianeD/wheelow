1f95a5c11ea63081f159e418438950a4b8f3eb50
Auréliane DOR <aureliane.dor@gmail.com>
2022-10-28 12:15:01 +0200

    feat: responsive

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,14 +2,14 @@
   <div class="bg-primary_1 pb-5">
     <div class="flex flex-row p-5 justify-between">
       <div>
-        <img src="~/assets/img/logo.png" alt="logo-wheelow" class="h-20 w-auto">
+        <img src="~/assets/img/logo-white.svg" alt="logo-wheelow" class="h-20 w-auto">
       </div>
       <div>
         <img src="~/assets/img/instagram.png" alt="instagram" class="h-7 w-auto mb-2">
         <img src="~/assets/img/facebook.png" alt="facebook" class="h-7 w-auto mt-2">
       </div>
     </div>
-    <p class="font-title text-center pb-10">Votre nouvelle ride commence ici !</p>
+    <p class="font-title text-center pb-10 text-2xl">Votre nouvelle ride commence ici !</p>
     <div class="flex justify-center pb-10">
       <Button title="Apple Store" class="bg-white text-primary_1 mr-2"/>
       <Button title="Play Store" class="bg-white text-primary_1 ml-2"/>
--- pages/index.vue
+++ pages/index.vue
@@ -1,94 +1,138 @@
 <template>
-  <div class="px-5 bg-white">
-    <div class="py-5 bg-white fixed w-screen z-10">
+  <div>
+    <div class="p-5 bg-white w-full fixed z-10 flex flex-row justify-between">
       <img src="~/assets/img/logo.png" alt="logo-wheelow" class="h-10 w-auto">
-      <h1 class="text-end font-title text-primary_1">WHEELOW</h1>
+      <h1 class=" font-title text-primary_1 text-3xl">WHEELOW</h1>
     </div>
-    <div class="flex flex-col justify-center pt-40 pb-10 md:flex-row">
-      <div class="w-fit">
-        <h1 class="font-title text-center text-3xl md:text-5xl">Votre nouvelle application pour rider un max</h1>
-        <p class="text-end font-text">by <span class="line-through">V3</span></p>
-        <p class="text-end font-title text-primary_1 text-xl">Wheelow</p>
+    <div class="px-5 bg-white">
+      <div class="flex flex-col justify-center pt-40 pb-10 md:flex-row">
+        <div class="w-fit md:w-50 md:flex md:flex-col md:justify-center md:items-center">
+          <h1 class="font-title text-center text-3xl md:text-5xl">Votre nouvelle application pour rider un max</h1>
+          <p class="text-end font-text md:pt-10">by <span class="line-through font-bold">V3</span></p>
+          <p class="text-end font-title text-primary_1 text-xl md:text-3xl">Wheelow</p>
+        </div>
+        <img src="~/assets/img/bike2.png" alt="image vélo" class=" my-10 md:pl-10">
       </div>
-      <img src="~/assets/img/bike2.png" alt="image vélo" class=" my-10">
-    </div>
-    <div class="flex flex-col items-center">
-      <Button class="my-10" title="Commencer à rider"/>
-    </div>
-    <h2 class="font-title text-3xl text-center py-10">Ce qui change</h2>
-    <p class="font-title text-center">Un design tout neuf, plus accessible</p>
-    <agile>
-      <div class="rounded-lg" >
-        <img src='~/assets/img/screen/homepage.png' alt="homepage" >
+      <div class="flex flex-col items-center">
+        <Button class="my-10" title="Commencer à rider"/>
       </div>
-      <div>
-        <img src='~/assets/img/screen/inscription.png' alt="inscription" >
+      <h2 class="font-title text-3xl text-center py-10">Ce qui change</h2>
+      <p class="font-title text-center text-primary_2 text-2xl py-20">Un design tout neuf, plus accessible</p>
+<!--      <agile infinite>-->
+<!--        <div>-->
+<!--          <img src='~/assets/img/screen/homepage.png' alt="homepage" class="pl-[50%]  h-96">-->
+<!--        </div>-->
+<!--        <div>-->
+<!--          <img src='~/assets/img/screen/station.png' alt="station" class="pl-[50%]  h-96">-->
+<!--        </div>-->
+<!--        <template v-slot:prevButton class="">-->
+<!--          <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="pb-10">-->
+<!--        </template>-->
+<!--        <template v-slot:nextButton>-->
+<!--          <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="rotate-180 pt-10">-->
+<!--        </template>-->
+<!--      </agile>-->
+      <div class="md:flex md:flex-row md:justify-around">
+        <div class="flex justify-center py-10">
+          <img src='~/assets/img/screen/homepage.png' alt="homepage" class="h-96 border-2 border-primary_1 p-2 rounded-lg">
+        </div>
+        <div class="flex justify-center pb-10 md:py-10">
+          <img src='~/assets/img/screen/station.png' alt="station" class="h-96 border-2 border-primary_1 p-2 rounded-lg">
+        </div>
       </div>
-      <div>
-        <img src='~/assets/img/screen/itineraire.png' alt="itineraire" >
+
+
+      <p class="font-title text-center text-primary_2 text-2xl py-20">Organiser simplement vos trajet du quotidien</p>
+<!--      <agile>-->
+<!--        <div class="rounded-lg" >-->
+<!--          <img src='~/assets/img/screen/trajet.svg' alt="trajet" class="h-50">-->
+<!--        </div>-->
+<!--        <div>-->
+<!--          <img src='~/assets/img/screen/vélo.svg' alt="vue velo" >-->
+<!--        </div>-->
+<!--        <div>-->
+<!--          <img src='~/assets/img/screen/pieton.svg' alt="vue pieton" >-->
+<!--        </div>-->
+<!--        <template v-slot:prevButton >-->
+<!--          <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="pb-10">-->
+<!--        </template>-->
+<!--        <template v-slot:nextButton>-->
+<!--          <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="rotate-180 pt-10">-->
+<!--        </template>-->
+<!--      </agile>-->
+      <div class="md:flex md:flex-row md:justify-around">
+        <div class="flex justify-center py-10">
+          <img src='~/assets/img/screen/trajet.svg' alt="trajet" class="h-96 border-2 border-primary_1 p-2 rounded-lg">
+        </div>
+        <div class="flex justify-center pb-10 md:py-10">
+          <img src='~/assets/img/screen/vélo.svg' alt="velo" class="h-96 border-2 border-primary_1 p-2 rounded-lg">
+        </div>
+        <div class="flex justify-center pb-10 md:py-10">
+          <img src='~/assets/img/screen/pieton.svg' alt="pieton" class="h-96 border-2 border-primary_1 p-2 rounded-lg">
+        </div>
       </div>
-      <template v-slot:prevButton >
-        <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="py-10">
-      </template>
-      <template v-slot:nextButton>
-        <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="rotate-180 py-10">
-      </template>
-    </agile>
-    <p class="font-title text-center">Organiser simplement vos trajet du quotidien</p>
-    <agile>
-      <div class="rounded-lg" >
-        <img src='~/assets/img/screen/trajet.svg' alt="trajet" >
+      <p class="font-title text-center text-primary_2 text-2xl py-20">Ne vous faites plus avoir, reservez votre vélo avant de partir</p>
+<!--      <agile>-->
+<!--        <div class="rounded-lg" >-->
+<!--          <img src='~/assets/img/screen/resa.svg' alt="reservation" >-->
+<!--        </div>-->
+<!--        <div>-->
+<!--          <img src='~/assets/img/screen/resa2.svg' alt="reservation" >-->
+<!--        </div>-->
+<!--        <template v-slot:prevButton >-->
+<!--          <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="py-10">-->
+<!--        </template>-->
+<!--        <template v-slot:nextButton>-->
+<!--          <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="rotate-180 py-10">-->
+<!--        </template>-->
+<!--      </agile>-->
+      <div class="md:flex md:flex-row md:justify-around">
+        <div class="flex justify-center py-10">
+          <img src='~/assets/img/screen/resa.svg' alt="reservation" class="h-96 border-2 border-primary_1 p-2 rounded-lg">
+        </div>
+        <div class="flex justify-center pb-10 md:py-10">
+          <img src='~/assets/img/screen/resa2.svg' alt="reservation" class="h-64 border-2 border-primary_1 p-2 rounded-lg">
+        </div>
+        <div class="flex justify-center pb-10 md:py-10">
+          <img src='~/assets/img/screen/resa3.svg' alt="reservation" class="h-72 border-2 border-primary_1 p-2 rounded-lg">
+        </div>
       </div>
-      <div>
-        <img src='~/assets/img/screen/vélo.svg' alt="vue velo" >
+
+      <p class="font-title text-center text-primary_2 text-2xl py-20">Faites du vélo, gagner des points...</p>
+<!--      <agile>-->
+<!--        <div class="rounded-lg" >-->
+<!--          <img src='~/assets/img/screen/1point.svg' alt="1 point" >-->
+<!--        </div>-->
+<!--        <div>-->
+<!--          <img src='~/assets/img/screen/2point.svg' alt="2 points" >-->
+<!--        </div>-->
+<!--        <div>-->
+<!--          <img src='~/assets/img/screen/3point.svg' alt="3 points" >-->
+<!--        </div>-->
+<!--        <template v-slot:prevButton >-->
+<!--          <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="py-10">-->
+<!--        </template>-->
+<!--        <template v-slot:nextButton>-->
+<!--          <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="rotate-180 py-10">-->
+<!--        </template>-->
+<!--      </agile>    -->
+      <div class="md:flex md:flex-row md:justify-around">
+        <div class="flex justify-center py-10">
+          <img src='~/assets/img/screen/1point.svg' alt="1 point" class="h-40 border-2 border-primary_1 p-2 rounded-lg">
+        </div>
+        <div class="flex justify-center pb-10 md:py-10">
+          <img src='~/assets/img/screen/2point.svg' alt="2 points" class="h-44 border-2 border-primary_1 p-2 rounded-lg">
+        </div>
+        <div class="flex justify-center pb-20 md:py-10">
+          <img src='~/assets/img/screen/3point.svg' alt="3 points" class="h-52 border-2 border-primary_1 p-2 rounded-lg">
+        </div>
       </div>
-      <div>
-        <img src='~/assets/img/screen/pieton.svg' alt="vue pieton" >
+      <h2 class="font-title text-3xl text-center">Et devenez le roi de la station</h2>
+      <div class="md:flex md:justify-center">
+        <img src="~/assets/img/bike.png" alt="vélo">
       </div>
-      <template v-slot:prevButton >
-        <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="py-10">
-      </template>
-      <template v-slot:nextButton>
-        <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="rotate-180 py-10">
-      </template>
-    </agile>
-    <p class="font-title text-center">Ne vous faites plus avoir, reservez votre vélo avant de partir</p>
-    <agile>
-      <div class="rounded-lg" >
-        <img src='~/assets/img/screen/resa.svg' alt="reservation" >
-      </div>
-      <div>
-        <img src='~/assets/img/screen/resa2.svg' alt="reservation" >
-      </div>
-      <template v-slot:prevButton >
-        <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="py-10">
-      </template>
-      <template v-slot:nextButton>
-        <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="rotate-180 py-10">
-      </template>
-    </agile>
-    <p class="font-title text-center">Faites du vélo, gagner des points...</p>
-    <agile>
-      <div class="rounded-lg" >
-        <img src='~/assets/img/screen/1point.svg' alt="1 point" >
-      </div>
-      <div>
-        <img src='~/assets/img/screen/2point.svg' alt="2 points" >
-      </div>
-      <div>
-        <img src='~/assets/img/screen/3point.svg' alt="3 points" >
-      </div>
-      <template v-slot:prevButton >
-        <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="py-10">
-      </template>
-      <template v-slot:nextButton>
-        <img src="~/assets/img/arrowOrange.svg" alt="arrowOrange" class="rotate-180 py-10">
-      </template>
-    </agile>    <h2 class="font-title text-3xl text-center">Et devenez le roi de la station</h2>
-    <img src="~/assets/img/bike.png" alt="vélo" >
-    <h2 class="font-title text-3xl text-center pt-10">Alors prêt à rider ?</h2>
-    <div class="flex justify-center my-10">
-<!--      <Button title="Télécharger l'application"/>-->
+      <h2 class="font-title text-3xl text-center py-20">Alors prêt à rider ?</h2>
+
     </div>
     <Footer />
   </div>
